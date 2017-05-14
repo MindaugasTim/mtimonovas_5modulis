07029abaeeabd6bde1f1df0163fa9aaebae1a803
Mindaugas <mindaugas.timonovas@gmail.com>
2017-05-14 20:50:42 +0300

    fixed but not working normal..

--- 012_uzduotis/src/spintele/uogiene.java
+++ 012_uzduotis/src/spintele/uogiene.java
@@ -27,42 +27,55 @@ public class uogiene {
 				System.out.println("Blogas kodas");
 			}
 		}
-		int j = 0;
+		int j = 0, x=1;
 		int mazy=0, karl=0, frek=0;
-		while(){						//???????????????????????
-			if(vizitai[j]==1){
+		while(x==1 && n>0 && m>=j+1 ){			
+			if(vizitai[j].equals(1)){
 				if(n==1){
 					mazy++;
-					j++;
+					n-=1;
+					x=0;
+					System.out.println("1if "+n);
 				}
+				if(n>1){
 				n -= 2;
 				mazy+=2;
 				j++;
+				System.out.println("1 "+n);
+				}
 			}
-			if(vizitai[j]==2){
+			if(vizitai[j].equals(2)){
 				if(n<=4){
-					j++;
 					switch (n){
-					case 1: karl+=1; n=0;
-					case 2: karl+=2; n=0;
-					case 3: karl+=3; n=0;
-					case 4: karl+=4; n=0;					
+					case 1: karl+=1; n=0; x=0; System.out.println("2if1 "+n); break;
+					case 2: karl+=2; n=0; x=0; System.out.println("2if2 "+n); break;
+					case 3: karl+=3; n=0; x=0; System.out.println("2if3 "+n); break;
+					case 4: karl+=4; n=0; x=0; System.out.println("2if4 "+n); break;				
 					}
 				}
+				if(n>4){
 				n-=5;
 				karl+=5;
 				j++;
+				System.out.println("2 "+n);
+				}
 			}
-			if(vizitai[j]==2){
+			if(vizitai[j].equals(3)){
 				if(n<=2){
 					switch (n){
-					case 1: frek+=1; n=0;
-					case 2: frek+=1; n=0;
+					case 1: frek+=1; n=0; x=0; System.out.println("3if1 "+n); break;
+					case 2: frek+=2; n=0; x=0; System.out.println("3if2 "+n); break;
 					}
 				}
+				if(n>2){
 				n-=3;
 				frek+=3;
 				j++;
+				System.out.println("3 "+n);
+				}
+			}
+			else if(n==0){
+				break;
 			}
 		}
 		if(n>0){
@@ -70,8 +83,8 @@ public class uogiene {
 		} else {
 			System.out.println("Uogienës neliko");
 		}
-		System.out.println("Maþyliui teko\t"+mazy+"\nKarlsonui\t"+karl+
-				"\nPanelei Frekenbok\t"+frek+"ðaukðt. uogienës.");
+		System.out.println("Maþyliui teko\t\t"+mazy+"\nKarlsonui\t\t"+karl+
+				"\nPanelei Frekenbok\t"+frek+"\tðaukðt. uogienës.");
 		
 	}
 }
